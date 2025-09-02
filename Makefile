5659278ba478319d9563e6ba78039bec67a3c5db
Jeremy-1991 <jeremyjammet@orange.fr>
2025-09-02 14:06:25 +0200

    Création api.py et modif Makefile

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@ SHELL := bash
 .ONESHELL:
 .SHELLFLAGS := -eu -o pipefail -c
 
+.PHONY: evaluations
+.PHONY: clean
+
 data:
 	mkdir data
 
-data/raw_dataset.csv: data
+models:
+	mkdir models
+
+data/raw_dataset.csv: | data
 	python -m src.load_data
 
 data/clean_dataset.csv: data/raw_dataset.csv
@@ -14,9 +20,11 @@ data/clean_dataset.csv: data/raw_dataset.csv
 data/preprocess_dataset.csv: data/clean_dataset.csv
 	python -m src.preprocess_data
 
-data/train_dataset.csv data/test_dataset.csv model.pkl scaler.pkl: data/preprocess_dataset.csv
+data/train_dataset.csv data/test_dataset.csv models/model.pkl models/scaler.pkl: data/preprocess_dataset.csv
 	python -m src.training
 
-.PHONY: evaluations
-evaluations: model.pkl scaler.pkl data/test_dataset.csv
+evaluations: models/model.pkl models/scaler.pkl data/test_dataset.csv
 	python -m src.evaluate
+
+clean:
+	rm -rf data models
---
+++ app/api.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+import numpy as np
+from src.utils import load_params
+
+# Charger les paramètres (chemins du modèle, scaler, dataset, etc.)
+params = load_params()
+MODEL_PATH = params["model"]["path"]
+SCALER_PATH = params["scaler"]["path"]
+
+# Charger modèle et scaler
+model = joblib.load(MODEL_PATH)
+scaler = joblib.load(SCALER_PATH)
+
+# Définir le schéma d'entrée pour les données
+class CustomerData(BaseModel):
+    Surname: str
+    CreditScore: int
+    Geography: str
+    Gender: str
+    Age: int
+    Tenure: int
+    Balance: float
+    NumOfProducts: int
+    HasCrCard: int
+    IsActiveMember: int
+    EstimatedSalary: float
+
+app = FastAPI(
+    title="Prédiction de Churn",
+    description="Application de prédiction de Churn <br>Une version par API pour faciliter la réutilisation du modèle",
+)
+
+
+# Endpoint de test
+@app.get("/")
+def root():
+    return {"message": "Bienvenue sur l'API de prédiction !"}
+
+# Endpoint de prédiction
+@app.post("/predict", tags=["Predict"])
+def predict(data: CustomerData) -> str:
+    df = pd.DataFrame([data.model_dump()]) # model_dump formatte les données pour pandas
+
+    preprocessed_data = preprocessor.transform(df)
+
+    prediction = (
+        "Exited" if int(model.predict(preprocessed_data)) == 1 else "Not Exited"
+    )
+
+    return prediction
